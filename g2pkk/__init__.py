d92b8c8678b3a605929786d2b9948f486f8adf2a
junyongleeme <leejunyong1150@gmail.com>
2022-08-25 11:06:39 +0900

    g2pk using komoran instead of mecab

---
+++ g2pkk/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+r"""g2pK
+"""
+from __future__ import absolute_import
+
+from .g2pkk import G2p
---
+++ test.py
@@ -0,0 +1,7 @@
+from g2pkk import G2p
+import time
+g2p = G2p()
+begin = time.time()
+print(g2p('나의 친구가 엠피쓰리 file 세개를 다운받고 있다'))
+end = time.time()
+print(end-begin)
